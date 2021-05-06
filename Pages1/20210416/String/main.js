7d59723c67eb7629d810da5a136ec265ac2fc6db
fwade0609 <fwade0609@gmail.com>
2021-05-06 15:02:11 +0800

    123
    456

--- Pages1/20210416/String/main.js
+++
@@ -1,8 +0,0 @@
-// 呼叫h1
-let thisH1 = document.getElementsByTagName("h1")[0];
-// showAlert函數被實作，
-thisH1.addEventListener("click",showAlert);
-
-function showAlert(){
-    alert(thisH1.innerHTML.split(" ")[1])
-}
